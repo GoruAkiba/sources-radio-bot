94d180e11b2fd733b92088d7abcc37167ef78486
GoruAkiba <goru.Akiba@gmail.com>
2020-07-01 18:54:53 +0700

    bug fixing

---
+++ webserver.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+const _PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/",(req,resp) => {
+	return resp.json({
+		status:"ok"
+	});
+})
+
+app.listen(_PORT, (e)=>{
+	console.log(`Listening to port : ${_PORT}`);
+})
